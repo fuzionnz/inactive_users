a755a15fb931e53e1e6e356f6f5c755ddaa0bdec
jitendrapurohit <jitendra@fuzion.co.nz>
2021-06-21 17:01:05 +0530

    fatal error fixes

--- src/Plugin/views/filter/EmailMatchViewsFilter.php
+++ src/Plugin/views/filter/EmailMatchViewsFilter.php
@@ -2,9 +2,7 @@
 
 namespace Drupal\inactive_users\Plugin\views\filter;
 
-use Drupal\views\Plugin\views\display\DisplayPluginBase;
 use Drupal\views\Plugin\views\filter\BooleanOperator;
-use Drupal\views\ViewExecutable;
 
 /**
  * Views filter for email mismatch.
@@ -21,10 +19,10 @@ class EmailMatchViewsFilter extends BooleanOperator {
   function query() {
     $this->ensureMyTable();
     if ($this->value) {
-      $this->query->addWhereExpression($this->options['group'], 'users_field_data.mail COLLATE utf8mb4_general_ci  IN (SELECT ' . $this->realField . ' COLLATE utf8mb4_general_ci FROM {' . $this->table . '} WHERE contact_id = civicrm_uf_match.contact_id)');
+      $this->query->addWhereExpression($this->options['group'], 'users_field_data.mail IN (SELECT ' . $this->realField . ' COLLATE utf8_unicode_ci FROM {' . $this->table . '} WHERE contact_id = civicrm_uf_match.contact_id)');
     }
     else {
-      $this->query->addWhereExpression($this->options['group'], 'users_field_data.mail COLLATE utf8mb4_general_ci  NOT IN (SELECT ' . $this->realField . ' COLLATE utf8mb4_general_ci FROM {' . $this->table . '} WHERE contact_id = civicrm_uf_match.contact_id)');
+      $this->query->addWhereExpression($this->options['group'], 'users_field_data.mail NOT IN (SELECT ' . $this->realField . ' COLLATE utf8_unicode_ci FROM {' . $this->table . '} WHERE contact_id = civicrm_uf_match.contact_id)');
     }
   }
 }
